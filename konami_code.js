32ff7d4b4f294c2aaf0a15c81c35cf750601a461
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-19 15:33:20 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  
   // Write your JavaScript code inside the init() function
   let index = 0;
  
@@ -20,4 +21,5 @@ function(e) {
     index = 0;
   }
 });
+
 }
